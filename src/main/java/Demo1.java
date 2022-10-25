129075060c63abd795b39a7502501b18ea266e98
hou721 <862650957@qq.com>
2022-10-25 19:38:45 +0800

    second

--- src/main/java/Demo1.java
+++ src/main/java/Demo1.java
@@ -3,6 +3,6 @@ public class Demo1 {
         System.out.println("helloworld");
         System.out.println("cao");
         System.out.println("服了");
-        System.out.println("xiaoxile");
+
     }
 }
