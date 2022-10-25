3dd5cdbdc6228f1580dcafb3cc14eb575beac369
hou721 <862650957@qq.com>
2022-10-25 19:37:06 +0800

    first

--- src/main/java/Demo1.java
+++ src/main/java/Demo1.java
@@ -2,5 +2,7 @@ public class Demo1 {
     public static void main(String[] args) {
         System.out.println("helloworld");
         System.out.println("cao");
+        System.out.println("服了");
+        System.out.println("xiaoxile");
     }
 }
